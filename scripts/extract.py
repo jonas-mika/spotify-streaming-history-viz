1bb48af505e56b982063ab223586ba8518b2f359
jonas-mika <accounts@jonassenghaas.de>
2022-04-25 22:01:43 +0200

    small changes to which metadata to extract

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,4 +1,13 @@
 import spotipy
+from pprint import pprint
+
+TRACK_FEATURES = [
+    "id",
+    "name",
+    "popularity",
+    "duration_ms",
+    "preview_url"
+    ]
 
 AUDIO_FEATURES = [
     "danceability",
@@ -13,23 +22,24 @@ AUDIO_FEATURES = [
     "valence",
     "tempo"]
 
-def get_track_data(track_name, artist_name, token):
+def get_track_data(track_name, artist_name, sp,
+    features=TRACK_FEATURES):
   try:
-    sp = spotipy.Spotify(auth=token)
     query=f"artist:{artist_name} "\
           f"track:{track_name}"
     search = sp.search(q=query, type="track")
     track_data = search['tracks']['items'][0]
-
-    return track_data
-  except: 
+    return {key:val for key, val in
+        track_data.items() if key in
+        features}
+  except:
     return None
 
-def get_track_features(track_id, token,
-    features=AUDIO_FEATURES):
-  try:
-    sp = spotipy.Spotify(auth=token)
-    track_features = sp.audio_features([trackId])[0]
-    return track_features[features]
-  except: 
+def get_track_features(track_id, sp, features=AUDIO_FEATURES):
+  try: 
+    track_features = sp.audio_features([track_id])[0]
+    return {key:val for key, val in
+        track_features.items() if key in
+        features}
+  except:
     return None
